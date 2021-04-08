41f00baeb512677f23445d27a00b82063c792e06
imkimura <juliakimura@univem.edu.br>
2021-04-07 21:53:27 -0300

    ...

--- matriz/main.py
+++ matriz/main.py
@@ -5,4 +5,13 @@ if __name__ == "__main__":
   
   m = Matriz([[0, 2, 1], [4, 5, 1], [8, 9, 1]])
   
-  print(m.rotacao(1,1,30))
+  print('---- Rotação -----')
+  print(m.rotacao(1, 1, 60))
+  print('')
+
+  print('---- Translação -----')
+  print(m.translacao(3, 4))
+  print('')
+  
+  print('---- Escalonamento -----')
+  print(m.escalonamento(3, 4))
